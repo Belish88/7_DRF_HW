388dff7ef4b364de3e9de5e8e9da965a6d13b506
Belish88 <belish888@rambler.ru>
2023-11-28 16:20:09 +0400

    add app: school, and add models: Course and Lesson

---
+++ school/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    img = models.ImageField(upload_to='course/', **NULLABLE, verbose_name='превью')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    img = models.ImageField(upload_to='course/', **NULLABLE, verbose_name='превью')
+    video = models.ImageField(upload_to='course/', **NULLABLE, verbose_name='видео')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
---
+++ school/urls.py
@@ -0,0 +1,7 @@
+from school.apps import SchoolConfig
+
+app_name = SchoolConfig.name
+
+urlpatterns = [
+
+]
