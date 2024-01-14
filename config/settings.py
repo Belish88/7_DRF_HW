56951b70eb67a8256aaeb407cd20dc516f422e84
Alex <belish888@rambler.ry>
2024-01-14 10:08:55 +0300

    celery

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_filters',
     'drf_yasg',
+    'django_celery_beat',
 
     'users',
     'school',
@@ -186,3 +187,10 @@ SIMPLE_JWT = {
 }
 
 STRIP_API_KEY = os.getenv('STRIP_API_KEY')
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  # URL-адрес брокера сообщений
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # URL-адрес брокера результатов, также Redis
+CELERY_TIMEZONE = 'UTC'  # Часовой пояс для работы Celery
+CELERY_TASK_TRACK_STARTED = True  # Флаг отслеживания выполнения задач
+CELERY_TASK_TIME_LIMIT = 30 * 60  # Максимальное время на выполнение задачи
